ede61b5ff5a932d0a7bd5976c2b7d06126ad78b7
Affandi Pratama <60037959+afanditama12@users.noreply.github.com>
2021-10-19 11:26:07 +0700

    first commit

---
+++ src/DetailTanaman.java
@@ -0,0 +1,15 @@
+public class DetailTanaman {
+    public static void main(String[] args) {
+        // buat objeck dengan variabel
+        var tanaman = new Tanaman(); 
+
+        // menampilkan method setter
+        tanaman.setNama("Mawar");
+        tanaman.setJenis("Tanaman Berduri");
+        tanaman.setCaraBerkembangBiak("Vegetatif");
+        tanaman.setTempatHidup("Daratan");
+
+        // menampilkan method getter
+        System.out.println("Nama Tanaman : " + tanaman.setNama("Mawar");
+    }
+}
---
+++ src/Tanaman.java
@@ -0,0 +1,39 @@
+public class Tanaman {
+    String nama;
+    String jenis;
+    String caraBerkembangBiak;
+    String tempatHidup;
+
+    void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    String getNama(String nama) {
+        return this.nama;
+    }
+
+    void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    String getJenis(String jenis) {
+        return this.jenis;
+    }
+
+    void setCaraBerkembangBiak(String caraBerkembangBiak) {
+        this.caraBerkembangBiak = caraBerkembangBiak;
+    }
+
+    String getCaraBerkembangBiak(String caraBerkembangBiak) {
+        return this.caraBerkembangBiak;
+    }
+
+    void setTempatHidup(String tempatHidup) {
+        this.tempatHidup = tempatHidup;
+    }
+
+    String getTempatHidup(String tempatHidup) {
+        return this.tempatHidup;
+    }
+
+}
